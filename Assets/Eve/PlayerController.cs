aa1a5b473acca86b2a1ced2dc8c807e6bc0f1505
ashlinaronin <ashlinaronin@gmail.com>
2021-04-03 11:33:12 -0700

    Fix avatar issues

--- Assets/Eve/PlayerController.cs
+++ Assets/Eve/PlayerController.cs
@@ -7,6 +7,21 @@ public class PlayerController : MonoBehaviour
 {
     Vector2 moveDirection;
     public float moveSpeed = 2;
+    public float maxForwardSpeed = 8;
+    float desiredForwardSpeed;
+    float forwardSpeed;
+
+    const float groundAcceleration = 5;
+    const float groundDeceleration = 25;
+
+    Animator animator;
+
+    bool IsMoveInput
+    {
+        get {
+            return !Mathf.Approximately(moveDirection.sqrMagnitude, 0f);
+        }
+    }
 
     public void OnMove(InputAction.CallbackContext context)
     {
@@ -15,14 +30,23 @@ public class PlayerController : MonoBehaviour
 
     void Move(Vector2 direction)
     {
-        // using direction.y to move character in z
-        transform.Translate(direction.x * moveSpeed * Time.deltaTime, 0, direction.y * moveSpeed * Time.deltaTime);
+        // clamp direction vector to magnitude of 1
+        if (direction.sqrMagnitude > 1f)
+        {
+            direction.Normalize();
+        }
+
+        desiredForwardSpeed = direction.magnitude * maxForwardSpeed;
+        float acceleration = IsMoveInput ? groundAcceleration : groundDeceleration;
+
+        forwardSpeed = Mathf.MoveTowards(forwardSpeed, desiredForwardSpeed, acceleration * Time.deltaTime);
+        animator.SetFloat("ForwardSpeed", forwardSpeed);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
