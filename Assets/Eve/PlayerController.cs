b7b55c360fe462c5aa6af88d7801750bdb6d0695
Ashlin Aronin <ashlinaronin@gmail.com>
2021-05-26 21:52:13 -0700

    Get (crappy) full jumping animation working

--- Assets/Eve/PlayerController.cs
+++ Assets/Eve/PlayerController.cs
@@ -14,12 +14,14 @@ public class PlayerController : MonoBehaviour
     float forwardSpeed;
     float jumpSpeed = 30000f;
     bool readyJump = false;
-
+    float groundRayDistance = 2f;
     const float groundAcceleration = 5;
     const float groundDeceleration = 25;
 
     Animator animator;
     Rigidbody rigidBody;
+
+    bool onGround = true;
 
     bool IsMoveInput
     {
@@ -62,7 +64,7 @@ public class PlayerController : MonoBehaviour
     {
         //Debug.Log(direction);
 
-        if (direction > 0)
+        if (direction > 0 && onGround)
         {
             animator.SetBool("ReadyJump", true);
             readyJump = true;
@@ -70,16 +72,26 @@ public class PlayerController : MonoBehaviour
         else if (readyJump)
         {
             animator.SetBool("Launch", true);
+            readyJump = false;
+            animator.SetBool("ReadyJump", false);
         }
     }
 
-    // todo: this isn't quite working-- she's not flying in the air
-    // like
+    // called from animation
     public void Launch()
     {
         rigidBody.AddForce(0, jumpSpeed, 0);
         animator.SetBool("Launch", false);
         animator.applyRootMotion = false;
+    }
+
+    // called from animation
+    public void Land()
+    {
+        animator.SetBool("Land", false);
+        animator.applyRootMotion = true;
+        animator.SetBool("Launch", false);
+        onGround = true;
     }
 
     // Start is called before the first frame update
@@ -94,5 +106,21 @@ public class PlayerController : MonoBehaviour
     {
         Move(moveDirection);
         Jump(jumpDirection);
+
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position + Vector3.up * groundRayDistance * 0.5f, -Vector3.up);
+        
+        if (Physics.Raycast(ray, out hit, groundRayDistance))
+        {
+            if (!onGround)
+            {
+                onGround = true;
+                animator.SetBool("Land", true);
+            }
+        } else {
+            onGround = false;
+        }
+
+        Debug.DrawRay(transform.position + Vector3.up * groundRayDistance * 0.5f, -Vector3.up * groundRayDistance, Color.red);
     }
 }
